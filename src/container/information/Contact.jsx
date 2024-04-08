189b267f9394cc0543e2196df0677e95c93ebe28
Rajani <rajanikrishnapr@gmail.com>
2024-04-08 17:14:35 +0530

    style changes

--- src/container/information/Contact.jsx
+++ src/container/information/Contact.jsx
@@ -43,7 +43,7 @@ export default function Contact() {
                     <div className="data-wrapper__mail">
                       <FaMailBulk
                         className="seven"
-                        size={40}
+                        size={30}
                         color="var(--yellow-theme-main-color)"
                       />
                       <p>{detailItem.email}</p>
@@ -51,7 +51,7 @@ export default function Contact() {
                     <div className="data-wrapper__number">
                       <FaMobile
                         className="one"
-                        size={40}
+                        size={30}
                         color="var(--yellow-theme-main-color)"
                       />
                       <p>{detailItem.phno}</p>
@@ -59,7 +59,7 @@ export default function Contact() {
                     <div className="data-wrapper__card">
                       <FaAddressCard
                         className="two"
-                        size={40}
+                        size={30}
                         color="var(--yellow-theme-main-color)"
                       />
                       <p>{detailItem.address}</p>
@@ -67,7 +67,7 @@ export default function Contact() {
                     <div className="data-wrapper__date">
                       <FaCalendar
                         className="three"
-                        size={40}
+                        size={30}
                         color="var(--yellow-theme-main-color)"
                       />
                       <p>{detailItem.dob}</p>
@@ -75,7 +75,7 @@ export default function Contact() {
                     <div className="data-wrapper__flag">
                       <FaFlag
                         className="four"
-                        size={40}
+                        size={30}
                         color="var(--yellow-theme-main-color)"
                       />
                       <p>{detailItem.nationality}</p>
@@ -83,7 +83,7 @@ export default function Contact() {
                     <div className="data-wrapper__gender-content">
                       <FaFemale
                         className="five"
-                        size={40}
+                        size={30}
                         color="var(--yellow-theme-main-color)"
                       />
                       <p>{detailItem.gender}</p>
@@ -91,7 +91,7 @@ export default function Contact() {
                     <div className="data-wrapper__profile">
                       <FaLinkedin
                         className="six"
-                        size={40}
+                        size={30}
                         color="var(--yellow-theme-main-color)"
                       />
                       <a
